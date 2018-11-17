201b36c27ea68cc028cbd6e8e4cb71c3d434cb8d
quynh <quinny.hcmc@gmail.com>
2018-11-17 14:04:01 +1100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,17 @@
+print('Hello Django Girls!')
+if 3 > 2:
+    print('It works')
+def hi():
+    print('Hi there')
+    print('How are you?')
+hi()
+def hi(name):
+        if name=='Ola'
+            print('Hi Ola!')
+        elif name=='Sonja'
+            print('Hi Sonja!')
+        else:
+            print('Hi anonymous!')
+
+hi()
+hi('Quynh')
